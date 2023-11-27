8d7e4b655a5a3a40ed9bbea2168ce81d1755bf0d
aittam9 <matt94pr@gmail.com>
2023-11-27 17:05:56 +0100

    adding .gitignore

--- aws_qas_dataset_builder.py
+++ aws_qas_dataset_builder.py
@@ -8,6 +8,8 @@ import os
 
 URL_ORIGIN = "https://aws.amazon.com/it/certification/certified-machine-learning-specialty/"
 
+#URL_ORIGIN_ENG = "https://aws.amazon.com/certification/certified-machine-learning-specialty/"
+
 # function to get all urls and store them in a dictionary
 def collect_urls(URL_ORIGIN):
     # create the bs object and parse the html
@@ -84,34 +86,42 @@ def compute_stats(all_qas):
     
     print(f"\nFor a total of {count} question-answer pair.")
 
-#TODO create a json builder
+#create a json builder
 def build_json(all_qas):
-    
-    pass
-#TODO create a csv builder
-def build_csv(all_qas):
     data_dir = "./Data"
     
-    print("Building CSV...")
-    df = pd.DataFrame(all_qas)
+    print("Building json file...")
     
     #check if data dir exists, if not create it
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
-    #save the csv
-    df.to_csv(data_dir+"/aws_qas.csv", sep = "\t")
-    print("...CSV done!")
-    print(df.head())
-    return df
+    #save the json file
+    with open("aws_qas.json", "w", encoding= "utf-8") as fw:
+        json.dump(all_qas, os.path.join(data_dir,fw))
+    print("...json file done!")
+    
+
+#TODO create an interaction module to get the desired language
+# def interaction():
+#     ask_user = input("Please choose a language, type it or eng:")
+#     if ask_user == "it":
+#         URL_ORIGIN = URL_ORIGIN
+#     elif ask_user == "eng":
+#         URL_ORIGIN == URL_ORIGIN_ENG
+#     else:
+#         print("Sorry, chosen language invalid or not supported")
+#     return URL_ORIGIN
+#     pass
 
 def main():
+    
     print(f"Scraping pages from {URL_ORIGIN}")
     URLS = collect_urls(URL_ORIGIN)
     
     all_qas = all_qas_collector(URLS)
     compute_stats(all_qas)
-
-    df = build_csv(all_qas)
+    
+    build_json(all_qas)
     
 
 if __name__ == "__main__":
